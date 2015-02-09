1516c2ae6fd2c2089239a74ebe3e619931875cf1
Federica Nocera <fnocera@uchicago.edu>
2015-02-09 11:52:15 -0600

    added functions to file

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -2,14 +2,45 @@ import requests
 from datetime import datetime
 
 def fetchAlbumIds(artist_id):
-    """Using the Spotify API, take an artist ID and 
-    returns a list of album IDs in a list
-    """
-    pass
+	'''Using the Spotify API take an artist_id and returns a list of 
+	albums_id in a list'''
+	url_base = 'https://api.spotify.com/v1/artists/'
+	artist_id_str = str(artist_id)
+	url = url_base + artist_id_str + '/albums?market=US&album_type=album'
+	#print url
+	req = requests.get(url)
+	if not req.ok:
+		print 'Error'
+	req_json = req.json()
+	get_items = req_json.get('items')
+	mylist = []
+	for i in range(len(get_items)):
+		get_line = get_items[i]
+		get_id = get_line['id']
+		mylist.append(get_id)
+
+	return mylist
+	
 
 def fetchAlbumInfo(album_id):
-    """Using the Spotify API, take an album ID 
-    and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
-    """
-    pass
+	url = 'https://api.spotify.com/v1/albums/' + album_id
+	req = requests.get(url)
+	if not req.ok:
+		print 'Error'
+	req_json = req.json()
+	get_artists = req_json.get('artists')
+	artist_id = get_artists[0]['id']
+	get_album_id = album_id
+	get_name = req_json.get('name')
+	get_year = req_json.get('release_date')
+	year = get_year[0:4]
+	get_popularity = req_json.get('popularity')
+	keys = ['artist_id', 'album_id', 'name', 'year', 'popularity']
+	values = [artist_id, album_id, get_name, year, get_popularity]
+	album_dict = dict(zip(keys,values))
+	return album_dict
+
+album_id = '6hU9JCoqq4GjYq86dQ1o9b'
+print(fetchAlbumInfo(album_id))
+
 
