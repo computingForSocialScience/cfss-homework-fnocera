0d8758ab73e36eeb9605866a41f1c677f1de1d04
Federica Nocera <fnocera@uchicago.edu>
2015-02-09 23:23:31 -0600

    added main function

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -7,6 +7,19 @@ from barChart import plotBarChart
 if __name__ == '__main__':
     artist_names = sys.argv[1:]
     print "input artists are ", artist_names
-    # YOUR CODE HERE
+    artist_list = []
+    albums_list= []
     
-
+    for name in artist_names:
+    	get_artist_id = fetchArtistId(name)
+    	#print get_artist_id
+    	get_artist_info = fetchArtistInfo(get_artist_id)
+    	get_album_ids = fetchAlbumIds(get_artist_id)
+    	artist_list.append(get_artist_info)
+    	for album in get_album_ids:
+    		get_album_info = fetchAlbumInfo(album)
+    		albums_list.append(get_album_info)
+	writeArtistsTable(artist_list)
+    writeAlbumsTable(albums_list)
+    plotBarChart()
+#print artist_list
