c5c9f7a4adfd440b86d26606ba17cbc17a1d4460
Federica Nocera <fnocera@uchicago.edu>
2015-02-09 23:23:03 -0600

    added functions that write artists and album lists to csv

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -15,20 +15,21 @@ def writeArtistsTable(artist_info_list):
         artist_name = line['name']
         artist_followers = str(line['followers'])
         artist_popularity = str(line['popularity'])
+        #print artist_id, artist_name, artist_followers, artist_popularity
         """f.write(artist_id)
         f.write(artist_name)
         f.write(artist_followers)
         f.write(artist_popularity)
         f.write('\n')
         f.write([artist_id, u',', artist_name, u',', artist_followers, u',', artist_popularity,u'\n'])"""
-
-        s=artist_id+','+artist_name+','+artist_followers+','+artist_popularity+'\n'
+        s=artist_id+','+'"'+artist_name+'"'+','+artist_followers+','+artist_popularity+'\n'
+        #print s
         f.write(s)
         
     f.close()
 
-artist_info_list = [{'genres': [u'riot grrrl'], 'popularity': 44, 'followers': 12431, 'id': u'0gvHPdYxlU94W7V5MSIlFe', 'name': u'Bikini Kill'}]
-writeArtistsTable(artist_info_list)
+#artist_info_list = [{'genres': [u'riot grrrl'], 'popularity': 44, 'followers': 12431, 'id': u'0gvHPdYxlU94W7V5MSIlFe', 'name': u'Bikini Kill'}]
+#writeArtistsTable(artist_info_list)
 
       
 def writeAlbumsTable(album_info_list):
@@ -48,10 +49,10 @@ def writeAlbumsTable(album_info_list):
         album_name = line['name']
         album_year = str(line['year'])
         album_popularity = str(line['popularity'])
-        s=artist_id+','+album_id+','+album_name+','+album_year+','+album_popularity+'\n'
+        s=artist_id+','+album_id+','+'"'+album_name+'"'+','+album_year+','+album_popularity+'\n'
         f.write(s)
         
     f.close()
 
-album_info_list = [{'popularity': 53, 'artist_id': u'6olE6TJLqED3rqDCT0FyPh', 'year': u'1993', 'name': u'In Utero - 20th Anniversary Super Deluxe', 'album_id': '14VVqBoDw1SxoNLW3Cj3mN'}]
-writeAlbumsTable(album_info_list)
+#album_info_list = [{'popularity': 53, 'artist_id': u'6olE6TJLqED3rqDCT0FyPh', 'year': u'1993', 'name': u'In Utero - 20th Anniversary Super Deluxe', 'album_id': '14VVqBoDw1SxoNLW3Cj3mN'}]
+#writeAlbumsTable(album_info_list)
